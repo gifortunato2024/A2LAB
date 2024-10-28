0dc5d00433b3fcbefcc9c1a6e70bc2017bcdd3ae
Giovanna Fortunato Pinto Santos <B435652@fgv.edu.br>
2024-10-28 12:40:01 -0300

    Inicia trabalho A2

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+import altair as alt
+
+#sidebar
+with st.sidebar:
+    st.sidebar.image("Coca-Cola_logo.svg", width=200) 
+    st.subheader("Sobre o nosso site")
+    st.write("""
+O site oferece gráficos e tabelas que avaliam as opiniões nas redes sociais sobre cada subsidiária da Coca-Cola Company, classificando-as como neutras, positivas ou negativas. Na aba Home, onde está disponível a visão geral da holding, você encontra todos os gráficos de forma condensada, proporcionando um panorama geral. 
+
+Ao selecionar uma subsidiária específica, os usuários podem explorar análises de sentimento mais detalhadas por meio de gráficos interativos, tabelas classificatórias e um sistema de monitoramento de crises que avalia a gravidade das situações enfrentadas por cada subempresa.
+
+Para uma gestão mais eficiente, contamos com um chatbot chamado E-Cris, nossa assistente virtual, que orienta sobre os tipos de crises e fornece informações sobre as melhores práticas de gerenciamento. O chatbot oferece fórmulas e exemplos de casos reais de crises, ajudando os gestores a tomarem decisões mais informadas.
+
+""")
+
+#Titulo
+st.markdown("<h1 style='color: red;'>The Coca-Cola Company</h1>", unsafe_allow_html=True)
+#subtitulo
+st.markdown("<h2 style='color: #000000; font-size: 24px; font-weight: bold;'>Escolha uma das subsidiárias para monitorar:</h2>", unsafe_allow_html=True)
+#box para selecionar a subsidiária
+option = st.selectbox(
+    "Qual subsidiária você quer monitorar?",
+    ("Todas", "fanta", "Coca-Cola", "Del-Valle", "Power-ade"),
+)
+
+st.write("Você escolheu:", option)
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
+
+
+
