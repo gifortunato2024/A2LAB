e95c24cc34c2cc7e5234fc19ea7bf38be4905e7e
JuliaFrazao <168451128+JuliaFrazao@users.noreply.github.com>
2024-11-15 21:20:53 -0300

    Update graficos.py

--- graficos.py
+++ graficos.py
@@ -15,25 +15,38 @@ from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.postprocessor import SimilarityPostprocessor
 from llama_index.core import Settings
 from llama_index.vector_stores.duckdb import DuckDBVectorStore
+import os
 
 # Baixar stopwords se necessário
 nltk.download('stopwords')
 
 # Configuração do chatbot
-import os 
 
 # Variáveis do ambiente
 with open('chave_groq', 'r') as arquivo:
     chave_groq = arquivo.read()
 groq_model = 'llama-3.2-90b-vision-preview'
-pasta_pdfs = 'pdfs.zip'
 modelo_embeddings = 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2'
 Settings.llm = Groq(api_key=chave_groq, model=groq_model, temperature=0)
 Settings.embed_model = HuggingFaceEmbedding(model_name=modelo_embeddings)
 Settings.chunk_size = 2048
 Settings.chunk_overlap = 512
 
-documents = SimpleDirectoryReader(pasta_pdfs).load_data()
+# Especificar diretamente os arquivos PDF
+pdf_files = [
+    'Material Gestão de Crise Trabalho Matheus.pdf',
+    'crise-pdf.pdf',
+    'crise_2_pdf.pdf',
+    'crise_3_pdf.pdf'
+]
+
+# Carregar todos os documentos dos arquivos especificados
+documents = []
+for file_path in pdf_files:
+    if os.path.exists(file_path):
+        documents.extend(SimpleDirectoryReader(file_path).load_data())
+    else:
+        st.warning(f"Arquivo {file_path} não encontrado.")
 
 # Criação do índice
 vector_store = DuckDBVectorStore('rag.duckdb', persist_dir='rag')
